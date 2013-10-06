ac0fa6535ea00e971945dcfeb69cbe4ef5652fe4
Andrew Szell <coldcue@gmail.com>
2013-10-06 13:45:33 +0200

    cntr_16 implemented

---
+++ fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "C:/dev/xilinx/mereshf/cntr_16_test_isim_beh.exe" -prj "C:/dev/xilinx/mereshf/cntr_16_test_beh.prj" "work.cntr_16_test" "work.glbl" 
