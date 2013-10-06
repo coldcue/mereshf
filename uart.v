7043673b45f88c989f1ee9f75acafc962df3dc1c
Andrew Szell <coldcue@gmail.com>
2013-10-06 14:06:32 +0200

    ASD

---
+++ uart.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    14:04:37 10/06/2013 
+// Design Name: 
+// Module Name:    uart 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module uart(
+    );
+
+
+endmodule
